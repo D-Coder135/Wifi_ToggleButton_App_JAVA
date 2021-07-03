480d8988c910c73de9364b2508d7f801063a55a1
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-03 23:28:48 +0530

    initialize the object of the class WifiManager with the method call of the method getApplicationContext() followed by getSystemService() method

--- app/src/main/java/com/example/wifi_togglebutton_app_java/MainActivity.java
+++ app/src/main/java/com/example/wifi_togglebutton_app_java/MainActivity.java
@@ -2,11 +2,15 @@ package com.example.wifi_togglebutton_app_java;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
     ToggleButton toggleButton;
+    Context context;
+    WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
